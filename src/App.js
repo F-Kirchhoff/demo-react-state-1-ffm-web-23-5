9447ee984ea1f3dab84d651085021b0d2ea32f3c
Felix Kirchhoff <felix.kirchhoff@neuefische.de>
2023-07-20 10:56:26 +0200

    add demo code

--- src/App.js
+++ src/App.js
@@ -1,9 +1,31 @@
+import { useState } from "react";
 import "./styles.css";
 
 export default function App() {
-  return <Counter />;
+  return (
+    <>
+      <Counter name="A" />
+      <Counter name="B" />
+      <Counter name="C" />
+    </>
+  );
 }
 
-function Counter() {
-  return <button>You have clicked this button 0 times</button>;
+function Counter({ name }) {
+  //start value of currentCount is 0 in this example
+  //currentCount can change over time if setCount is used!
+  const [currentCount, setCount] = useState(0);
+
+  return (
+    <button
+      onClick={() => {
+        //  ~== button.addEventlistener('click')
+
+        setCount(currentCount + 1); // trigger rerender when calling the setter function
+        console.log(`Button ${name} value:`, currentCount);
+      }}
+    >
+      You have clicked this button {currentCount} times
+    </button>
+  );
 }
